330f121d23c63bf26856006b0e8b66302d63d796
Sally Lundy <slundy123456@gmail.com>
2023-03-06 01:29:47 +0100

    unittest for die class

---
+++ test_die.py
@@ -0,0 +1,22 @@
+import unittest
+from random import randint 
+from unittest.mock import patch
+from die import Die
+
+class TestDie(unittest.TestCase):
+    @patch('die.randint', return_value=6)
+    def test_die(self, mock_randint):
+        die = Die()
+        self.assertEqual(str(die),"6")
+
+#from random import randint
+
+#class Die:
+#    def __init__(self):
+#        self.value = randint(1,6)
+
+#    def __str__(self):
+#        return f"{self.value}"
+
+if __name__ == '__main__':
+    unittest.main()
