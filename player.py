0ce62e5fe57e75d6ee79f533087bcceda2814241
sonejoel560 <soneemilejoel@gmail.com>
2023-03-08 22:54:47 +0100

    Modified player.py

--- player.py
+++ player.py
@@ -13,19 +13,19 @@ class Player:
     
     def round(self):
         choice = "roll"
-        while choice != "hold":
-            choice = input("Do you want to roll or hold?")
-            if choice == "hold":
+        while choice != "h":
+            choice = input("Do you want to roll (r) or hold (h)? ")
+            if choice == "h":
+                print("Your score for this round is ", self.roundScore)
                 break
             roll = Die().value
             print("You rolled", roll)
             if roll != 1:
                 self.roundScore += roll
-                print("Your score for this round is ", self.roundScore)
             else:
                 print("You pigged it!")
                 self.roundScore = 0
                 break
         self.playerScore += self.roundScore
-        print("Your toatal score is ", self.playerScore)
+        print("Your total score is ", self.playerScore)
         self.roundScore = 0
