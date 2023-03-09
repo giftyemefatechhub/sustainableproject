e5f0536feb2c2debd43b33fa6183580661778e2a
giftyemefatechhub <126504340+giftyemefatechhub@users.noreply.github.com>
2023-03-09 09:36:39 +0100

    Add files via upload

---
+++ sustainableproject/unittests/test_die.py
@@ -0,0 +1,15 @@
+import unittest
+from random import randint 
+from unittest.mock import patch
+from guess.die import Die
+
+class TestDie(unittest.TestCase):
+    @patch('die.randint', return_value=6)
+    def test_die(self, mock_randint):
+        die = Die()
+        self.assertEqual(str(die),"6")
+
+
+
+if __name__ == '__main__':
+    unittest.main()
