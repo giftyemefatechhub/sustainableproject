a6e090533d567a94fb62bfa0f595f46787b7f688
sonejoel560 <soneemilejoel@gmail.com>
2023-04-13 04:18:45 +0200

    Modified Player class: Added rename() function

--- player.py
+++ player.py
@@ -1,30 +1,48 @@
 from dice import Dice
+from highscore import Highscore
 
 
 class Player:
     def __init__(self, name):
         self.name = name
-        self.roundScore = 0
-        self.playerScore = 0
-    
+        self.round_score = 0
+        self.player_score = 0
+        self.highscore = Highscore()
+
+
     def __str__(self):
         return f"{self.name}"
-    
+
     def round(self):
         choice = input("Type (r) to roll: ")
         while choice != "h":
-            roll = Dice().value
+            '''
+            Cheat code
+            '''
+            if choice == "roll":
+                roll = Dice().value * 2
+            else:
+                roll = Dice().value
             print(f"{self.name} rolled {roll}")
             if roll != 1:
-                self.roundScore += roll
+                self.round_score += roll
                 choice = input("Would you like to roll again (r) or hold (h)? ")
             else:
                 print(f"{self.name} rolled a 1 and pigged this round!")
-                self.roundScore = 0
+                self.round_score = 0
                 break
             if choice == "h":
                 break
-        print(f"{self.name}'s scored {self.roundScore} for this round")
-        self.playerScore += self.roundScore
-        print(f"{self.name}'s total score: {self.playerScore}")
-        self.roundScore = 0
+        print(f"{self.name}'s scored {self.round_score} for this round")
+        self.player_score += self.round_score
+        print(f"{self.name}'s total score: {self.player_score}")
+        self.round_score = 0
+
+    def rename(self):
+        new_name = input(f"{self.name} would you like to change your name? (y/n) ")
+        if new_name.lower() == "y":
+            old_name = self.name
+            self.name = input("Enter new name: ")
+            print(f"{old_name} changed name to {self.name}")
+        else:
+            print("Name remain unchanged")
