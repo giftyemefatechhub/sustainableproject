b71a4010eddd80d8d564e22e71487f5776528033
sonejoel560 <soneemilejoel@gmail.com>
2023-04-13 04:22:55 +0200

    Unchanged recommiting to make sure file is updated

--- dice.py
+++ dice.py
@@ -3,7 +3,7 @@ from random import randint
 
 class Dice:
     def __init__(self):
-        self.value = randint(1,6)
+        self.value = randint(1, 6)
 
     def __str__(self):
         return f"{self.value}"
